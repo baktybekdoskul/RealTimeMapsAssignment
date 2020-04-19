e11d1d2b968c5fddb31eb3a776a28279b6475ad1
baktybekdoskul <baktybek.doskul@gmail.com>
2020-04-19 17:05:55 +0600

    OnMapClick dialog finished

--- app/src/main/java/com/example/real_timemapsassignment/MapsFragment.java
+++ app/src/main/java/com/example/real_timemapsassignment/MapsFragment.java
@@ -1,8 +1,10 @@
 package com.example.real_timemapsassignment;
 
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -15,8 +17,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
-public class MapsFragment extends Fragment implements OnMapReadyCallback {
+public class MapsFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap;
 
@@ -44,10 +47,36 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMapClickListener(this);
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Toast.makeText(getActivity().getApplicationContext(), "map clicked", Toast.LENGTH_SHORT).show();
+        new MaterialAlertDialogBuilder(getContext())
+                .setTitle("Marker addition")
+                .setMessage("Will you save this place as a marker?")
+                .setPositiveButton("Yes, I will", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity().getApplicationContext(), "saved", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No, I won't", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity().getApplicationContext(), "cancelled", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
+
+    }
+
+
 }
